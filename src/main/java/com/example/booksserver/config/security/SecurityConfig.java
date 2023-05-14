6dec5a651fdc2ebdec1d068a52d525fb75698b9b
Artyom Belsky <artyombel2003@gmail.com>
2023-05-14 17:18:09 +0300

    Users are authenticated now. Creation of Books and Authors is secure. No changes in OpenAPI spec. yet

---
+++ src/main/java/com/example/booksserver/config/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.booksserver.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/authors**").hasRole(ResourceRole.ADMIN.name())
+                        .requestMatchers(HttpMethod.POST, "/books**").hasRole(ResourceRole.ADMIN.name())
+                        .anyRequest().permitAll()
+                );
+
+        http.oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt
+                        .jwtAuthenticationConverter(new JwtWithRolesConverterFactory("books-server").createConverter())));
+        return http.build();
+    }
+}
