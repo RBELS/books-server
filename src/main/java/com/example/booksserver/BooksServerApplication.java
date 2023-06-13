ee2638d7c3203dc6f11ab190cb3f754a12e69936
Artyom Belsky <artyombel2003@gmail.com>
2023-06-14 00:01:50 +0300

    Async tasks are not used now

--- src/main/java/com/example/booksserver/BooksServerApplication.java
+++ src/main/java/com/example/booksserver/BooksServerApplication.java
@@ -2,11 +2,9 @@ package com.example.booksserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableAsync
 @SpringBootApplication
 public class BooksServerApplication {
     public static void main(String[] args) {
