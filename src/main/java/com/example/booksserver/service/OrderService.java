d5e31921b3e7de466aaab914675bd98c4a9c16ac
Artyom Belsky <artyombel2003@gmail.com>
2023-06-20 15:29:16 +0300

    Redunant static method deleted from OrderService class

--- src/main/java/com/example/booksserver/service/OrderService.java
+++ src/main/java/com/example/booksserver/service/OrderService.java
@@ -9,13 +9,4 @@ public interface OrderService {
     Order createOrder(Order order, CardInfo cardInfo) throws ResponseStatusException;
     Order cancelOrder(Long orderId) throws ResponseStatusException;
     Order getOrderById(Long orderId) throws ResponseStatusException;
-
-    static void moveStock(Order order, boolean isReverse) {
-        order.getOrderItems().forEach(orderItemDto -> {
-            Stock stock = orderItemDto.getBook().getStock();
-            int moveModifier = isReverse ? -1 : 1;
-            stock.setAvailable(stock.getAvailable() - moveModifier * orderItemDto.getCount());
-            stock.setInDelivery(stock.getInDelivery() + moveModifier * orderItemDto.getCount());
-        });
-    }
 }
